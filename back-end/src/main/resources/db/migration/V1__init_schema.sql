ef85d94d8a7f2b9fe6cb86a83a9f3d607146d896
Jovan Dimov <jovandimov@gmail.com>
2022-07-19 13:56:47 +0200

    add questions,tags and users

--- back-end/src/main/resources/db/migration/V1__init_schema.sql
+++ back-end/src/main/resources/db/migration/V1__init_schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE AppUser
+CREATE TABLE app_users
 (
     id            bigserial primary key,
     username      text not null,
@@ -15,10 +15,29 @@ CREATE TABLE activation_tokens
     constraint fk_activation_tokens_app_user_id foreign key (app_user_id) references app_users (id)
 );
 
-CREATE TABLE Question
+CREATE TABLE question
 (
-    id bigserial primary key,
-    question_text text not null,
-    
+    id                 bigserial primary key,
+    title              text   not null,
+    question_text      text   not null,
+    parent_question_id bigint default null,
+    app_user_id        bigint not null,
+    constraint fk_question_user_id foreign key (app_user_id) references app_users (id),
+    constraint fk_question_parent_question_id foreign key (parent_question_id) references question (id)
+);
 
+CREATE TABLE tag
+(
+    id          bigserial primary key,
+    name        text not null,
+    description text not null
+);
+
+CREATE TABLE question_tag
+(
+    id          bigserial primary key,
+    question_id bigint not null,
+    tag_id      bigint not null,
+    constraint fk_question_question_id foreign key (question_id) references question (id),
+    constraint fk_tag_tag_id foreign key (tag_id) references tag (id)
 );
